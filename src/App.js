360c70c816eb6ef4a2d6792cc9f9e4c22297d862
Sayedur Rahman <s.rahman3443@gmail.com>
2022-12-13 11:22:57 +0600

    project added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import React, { useContext } from "react";
-
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
-import AuthContext from "./store/auth-context";
-
 function App() {
-	const ctx = useContext(AuthContext);
-
-	return (
-		<>
-			<MainHeader />
-			<main>
-				{!ctx.isLoggedIn && <Login />}
-				{ctx.isLoggedIn && <Home />}
-			</main>
-		</>
-	);
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
+  );
 }
 
 export default App;
