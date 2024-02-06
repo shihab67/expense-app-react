3cf48a8fef9d4729b5a69db723089bca3b9403dc
Sayedur Rahman <s.rahman3443@gmail.com>
2024-02-06 12:42:41 +0600

    project added

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
-import "./App.css";
-import { ExpenseItem } from "./components/ExpenseItem/ExpenseItem";
-import { Container } from "react-bootstrap";
-import { useState } from "react";
-
-const list_expenses = [
-	{
-		id: 1,
-		date: "2022-05-01",
-		title: "Toilet Paper",
-		amount: 40,
-	},
-	{
-		id: 2,
-		date: "2022-05-02",
-		title: "ladies Shoe",
-		amount: 100,
-	},
-];
+import React from 'react';
+import BackwardCounter from './components/BackwardCounter';
+import ForwardCounter from './components/ForwardCounter';
 
 function App() {
-	const [expenses, setExpenses] = useState(list_expenses);
-
-	const handleExpense = (expense) => {
-		setExpenses((previousExpesne) => {
-			return [...previousExpesne, expense];
-		});
-	};
-
-	return (
-		<div className="App">
-			<Container>
-				<ExpenseItem
-					onSaveExpense={handleExpense}
-					expenses={expenses}
-				/>
-			</Container>
-		</div>
-	);
+  return (
+    <React.Fragment>
+      <ForwardCounter />
+      <BackwardCounter />
+    </React.Fragment>
+  );
 }
 
 export default App;
