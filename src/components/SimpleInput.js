204e572726b441d1dc5ae3ddaf693a6588f2cb97
Sayedur Rahman <s.rahman3443@gmail.com>
2024-02-12 16:53:31 +0600

    added some code

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,38 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+	const [enteredName, setEnteredName] = useState("");
+	const [enteredNameisValid, setEnteredNameisValid] = useState(true);
+	const nameChangeHandler = (event) => {
+		setEnteredName(event.target.value);
+	};
+
+	const formSubmissionHandler = (event) => {
+		event.preventDefault();
+
+		if (enteredName.trim() === "") {
+			setEnteredNameisValid(true);
+			return;
+		}
+
+		setEnteredName(true);
+	};
+
+	return (
+		<form onSubmit={formSubmissionHandler}>
+			<div className="form-control">
+				<label htmlFor="name">Your Name</label>
+				<input type="text" id="name" onChange={nameChangeHandler} />
+
+				{!enteredNameisValid && (
+					<span className="error-text">Name field is required!</span>
+				)}
+			</div>
+			<div className="form-actions">
+				<button>Submit</button>
+			</div>
+		</form>
+	);
 };
 
 export default SimpleInput;
