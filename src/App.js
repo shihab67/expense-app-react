bf0252a5ea7ca3348729f8f85e5171e5a8d5c80c
Sayedur Rahman <s.rahman3443@gmail.com>
2024-02-22 10:38:10 +0600

    project added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import { Header } from "./components/Layout/Header";
-import { Meals } from "./components/Meals/Meals";
-import { CartProvider } from "./store/CartProvider";
+import Counter from './components/Counter';
+
 
 function App() {
-	return (
-		<CartProvider>
-			<Header />
-			<div className="container pt-4 d-flex justify-content-center align-items-center">
-				<Meals />
-			</div>
-		</CartProvider>
-	);
+  return (
+    <Counter />
+  );
 }
 
 export default App;
