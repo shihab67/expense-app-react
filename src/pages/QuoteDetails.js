a1150836d193a2db770c4a93f881f7dc48c6da63
Sayedur Rahman <s.rahman3443@gmail.com>
2024-02-26 22:50:02 +0600

    Update QuoteDetails.js

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,8 +1,17 @@
-import { useParams } from "react-router-dom";
+import { Route, useParams } from "react-router-dom";
+import Comments from "../components/comments/Comments";
 
 function QuoteDetails() {
 	const params = useParams();
-	return <div>QuoteDetails</div>;
+	return (
+		<>
+			<div>QuoteDetails</div>
+			<p>{params.quoteId}</p>
+			<Route path={`/quotes/${params.quoteId}/comments`}>
+				<Comments />
+			</Route>
+		</>
+	);
 }
 
 export default QuoteDetails;
