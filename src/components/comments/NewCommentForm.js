5dd62f5134c89598722f736add095a982b003ad8
Sayedur Rahman <s.rahman3443@gmail.com>
2024-02-27 23:38:16 +0600

    project completed

--- src/components/comments/NewCommentForm.js
+++ src/components/comments/NewCommentForm.js
@@ -1,29 +1,50 @@
-import { useRef } from 'react';
+import { useEffect, useRef } from "react";
+import useHttp from "../../hooks/use-http";
+import { addComment } from "../../lib/api";
 
-import classes from './NewCommentForm.module.css';
+import LoadingSpinner from "../UI/LoadingSpinner";
+import classes from "./NewCommentForm.module.css";
 
 const NewCommentForm = (props) => {
-  const commentTextRef = useRef();
+	const commentTextRef = useRef();
+	const { sendRequest, status, error } = useHttp(addComment);
+	const { onAddedComment } = props;
 
-  const submitFormHandler = (event) => {
-    event.preventDefault();
+	useEffect(() => {
+		if (status === "completed" && !error) {
+			onAddedComment();
+		}
+	}, [status, error, onAddedComment]);
 
-    // optional: Could validate here
+	const submitFormHandler = (event) => {
+		event.preventDefault();
 
-    // send comment to server
-  };
+		// optional: Could validate here
 
-  return (
-    <form className={classes.form} onSubmit={submitFormHandler}>
-      <div className={classes.control} onSubmit={submitFormHandler}>
-        <label htmlFor='comment'>Your Comment</label>
-        <textarea id='comment' rows='5' ref={commentTextRef}></textarea>
-      </div>
-      <div className={classes.actions}>
-        <button className='btn'>Add Comment</button>
-      </div>
-    </form>
-  );
+		sendRequest({
+			commentData: {
+				text: commentTextRef.current.value,
+			},
+			quoteId: props.quoteId,
+		});
+	};
+
+	return (
+		<form className={classes.form} onSubmit={submitFormHandler}>
+			{status === "pending" && (
+				<div className="centered">
+					<LoadingSpinner />
+				</div>
+			)}
+			<div className={classes.control} onSubmit={submitFormHandler}>
+				<label htmlFor="comment">Your Comment</label>
+				<textarea id="comment" rows="5" ref={commentTextRef}></textarea>
+			</div>
+			<div className={classes.actions}>
+				<button className="btn">Add Comment</button>
+			</div>
+		</form>
+	);
 };
 
 export default NewCommentForm;
--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,31 +1,48 @@
+import { useEffect } from "react";
 import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-const DUMMY_QUOTES = [
-	{
-		id: "q1",
-		author: "Max",
-		text: "Learning React is fun!",
-	},
-	{
-		id: "q2",
-		author: "Maximilan",
-		text: "Learning React is great!",
-	},
-];
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
 
 function QuoteDetails() {
 	const params = useParams();
 	const match = useRouteMatch();
-	const quote = DUMMY_QUOTES.find((q) => q.id === params.quoteId);
+	const { quoteId } = params;
+	const {
+		sendRequest,
+		status,
+		data: loadedQuote,
+		error,
+	} = useHttp(getSingleQuote, true);
 
-	if (!quote) {
+	useEffect(() => {
+		sendRequest(quoteId);
+	}, [sendRequest, quoteId]);
+
+	if (status === "pending") {
+		return (
+			<div className="centered">
+				<LoadingSpinner />
+			</div>
+		);
+	}
+
+	if (error) {
+		return <p className="centered">{error}</p>;
+	}
+
+	if (!loadedQuote.text) {
 		return <p>No quote found!</p>;
 	}
 
 	return (
 		<>
-			<HighlightedQuote text={quote.text} author={quote.author} />
+			<HighlightedQuote
+				text={loadedQuote.text}
+				author={loadedQuote.author}
+			/>
 			<Route path={match.path} exact>
 				<div className="centered">
 					<Link className="btn--flat" to={`${match.url}/comments`}>
