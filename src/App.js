187eb5c11d6ad7a717dfc512256e70e7c7804858
Sayedur Rahman <s.rahman3443@gmail.com>
2024-02-28 23:09:04 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Switch, Route } from 'react-router-dom';
 
-import Layout from './components/Layout/Layout';
+import Layout from './components/layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
