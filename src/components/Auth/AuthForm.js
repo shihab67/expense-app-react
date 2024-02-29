73fa16e1dae5ac5f11156fdd45c2b510d8a02fae
Sayedur Rahman <s.rahman3443@gmail.com>
2024-02-29 15:32:00 +0600

    added signup, sign in code

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -6,22 +6,67 @@ const AuthForm = () => {
 	const emailInputRef = useRef();
 	const passwordInputRef = useRef();
 	const [isLogin, setIsLogin] = useState(true);
+	const [isLoading, setIsLoading] = useState(false);
 
 	const switchAuthModeHandler = () => {
 		setIsLogin((prevState) => !prevState);
 	};
 
 	const submitFormHandler = (event) => {
-		event.prevetDefault();
+		event.preventDefault();
 
 		const email = emailInputRef.current.value;
 		const password = passwordInputRef.current.value;
+		let url = "";
+
+		setIsLoading(true);
+
+		if (isLogin) {
+			url =
+				"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBNAeuyayYTAnKvNrMxsMeHat--jzsEHKA";
+		} else {
+			url =
+				"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBNAeuyayYTAnKvNrMxsMeHat--jzsEHKA";
+		}
+
+		fetch(url, {
+			method: "POST",
+			body: JSON.stringify({
+				email: email,
+				password: password,
+				returnSecureToken: true,
+			}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((res) => {
+				setIsLoading(false);
+
+				if (res.ok) {
+					return res.json();
+				} else {
+					return res.json().then((data) => {
+						let errorMessage = "Authentication failed!";
+						if (data && data.error && data.error.message) {
+							errorMessage = data.error.message;
+						}
+						throw new Error(errorMessage);
+					});
+				}
+			})
+			.then((data) => {
+				console.log(data);
+			})
+			.catch((error) => {
+				alert(error.message);
+			});
 	};
 
 	return (
 		<section className={classes.auth}>
 			<h1>{isLogin ? "Login" : "Sign Up"}</h1>
-			<form>
+			<form onSubmit={submitFormHandler}>
 				<div className={classes.control}>
 					<label htmlFor="email">Your Email</label>
 					<input
@@ -41,7 +86,12 @@ const AuthForm = () => {
 					/>
 				</div>
 				<div className={classes.actions}>
-					<button>{isLogin ? "Login" : "Create Account"}</button>
+					{!isLoading && (
+						<button type="submit">
+							{isLogin ? "Login" : "Create Account"}
+						</button>
+					)}
+					{isLoading && <p>Sending request...</p>}
 					<button
 						type="button"
 						className={classes.toggle}
